e3d3a4519632be5fa70393231f2a8b48091ce695
Shchemelyov Egor <egor2014ag@mail.ru>
2024-04-04 11:26:28 +0300

    full create

--- core/assistenet/model.py
+++ core/assistenet/model.py
@@ -9,7 +9,7 @@ class OpenAI_Assistent:
             api_key=os.environ.get("OPENAI_API_KEY", api_key),
         )
 
-    def create_assistent(self):
+    def create_assistent(self, name=None, instructions=None, model=None):
         return self.client.beta.assistants.create(
                 name="Леночка",
                 instructions="Добрый  день, я представляю группу компаний БИТУМ ЭКСПРЕС В ВАШ ДОМ. Мы производим высоскокачественный битум. Скажите вам это интересно?",
@@ -20,11 +20,29 @@ class OpenAI_Assistent:
         return self.client.beta.threads.create()
     
     def message_create(self, thread, user_id:str, message:str):
-        pass
+        
+        return self.client.beta.threads.messages.create(
+                    thread_id=thread.id,
+                    user_id=user_id,
+                    message=message
+                )
     
     def send_message(self, thread, assistant):
         run = self.client.beta.threads.runs.create(
                     thread_id=thread.id,
                     assistant_id=assistant.id,
                 )
+        return self.wait_on_run(
+                    run=run,
+                    thread=thread
+                         ) 
+    
+    def wait_on_run(self, run, thread):
+        while run.status == "queud" and run.status == "in_progress":
+            run = self.client.beta.threads.runs.retrieve(
+                thread_id=thread.id,
+                run_id=run.id
+            )
+            time.sleep(.5)
+            
         return run
