0ac4df9df521b00dd22bc1d508a905e68af59dee
Egor Shchemelyov <egor2014ag@mail.ru>
2024-04-04 08:46:23 +0300

    add class

--- core/model.py
+++ core/model.py
@@ -1,17 +1,30 @@
 import os
+import time
 from openai import OpenAI
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
+class OpenAI_Assistent:
+    def __init__(self, api_key:str):
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-4",
-)
+        self.client = OpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY", api_key),
+        )
+
+    def create_assistent(self):
+        return self.client.beta.assistants.create(
+                name="Леночка",
+                instructions="Добрый  день, я представляю группу компаний БИТУМ ЭКСПРЕС В ВАШ ДОМ. Мы производим высоскокачественный битум. Скажите вам это интересно?",
+                model="gpt-4"
+            )
+    
+    def create_thread(self):
+        return self.client.beta.threads.create()
+    
+    def message_create(self, thread, user_id:str, message:str):
+        pass
+    
+    def send_message(self, thread, assistant):
+        run = self.client.beta.threads.runs.create(
+                    thread_id=thread.id,
+                    assistant_id=assistant.id,
+                )
+        return run
